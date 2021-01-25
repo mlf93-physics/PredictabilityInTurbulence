e37a9622355934c98130e11b4601f253f986ab24
Martin Frølund <mlf@perfusiontech.com>
2021-01-25 15:43:02 +0100

    Initial commit

---
+++ sabra_model/params.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+# Initialise model
+epsilon = 0.5
+lambda_const = 2
+ny = 0
+dt = 1e-7
+time_to_run = 1.0   # [s]
+L = 2*np.pi
+Nt = int(time_to_run/dt)
+sample_rate = 1/50
+n_k_vec = 20
+Nx = int(lambda_const**n_k_vec)
+u0 = 1
+forcing = 0
+n_forcing = 0
+
+factor2 = - epsilon/lambda_const
+factor3 = (1 - epsilon)/lambda_const**2
+
+# Define k vector indices
+k_vec_temp = np.array([lambda_const**(n + 1) for n in range(n_k_vec)], dtype=np.int64)
+
---
+++ sabra_model/sabra_model.py
@@ -0,0 +1,76 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+from math import floor, log, ceil, sqrt
+import sys
+sys.path.append('..')
+from burgers_eq.runge_kutta4 import runge_kutta4_vec
+from params import *
+
+def run_model():
+    sample_number = 0
+
+    x_array = np.linspace(-L/2, L/2, Nx + 1, endpoint=False)
+    data_out = np.zeros((int(Nt*sample_rate), n_k_vec + 1), dtype=np.complex)
+    forcing_array = np.zeros(n_k_vec, dtype=np.complex)
+    forcing_array[n_forcing] = forcing
+        
+    initial_k_vec = k_vec_temp**(-1/3)
+    u_old = (u0*initial_k_vec).\
+        astype(np.complex)
+
+    # Pad arrays
+    bd_size = 2
+    u_old = np.pad(u_old, pad_width=bd_size, mode='constant')
+
+    def derivative_evaluator(time=None, u_old=None):
+        pre_factor = 1j*k_vec_temp
+        du = pre_factor * ( u_old.conj()[bd_size+1:-bd_size+1]*
+                            u_old[bd_size+2:] +
+                            factor2*u_old.conj()[bd_size-1:-bd_size-1]*
+                            u_old[bd_size+1:-bd_size+1] +
+                            factor3*u_old[:-bd_size-2]*
+                            u_old[bd_size-1:-bd_size-1] ) \
+             - ny*k_vec_temp**2*u_old[bd_size:-bd_size] + forcing_array
+
+        du = np.pad(du, pad_width=bd_size, mode='constant')
+        
+        return du
+
+    # Perform calculations
+    for i in range(Nt):
+        u_new = runge_kutta4_vec(y0=u_old, h=dt, dydx=derivative_evaluator)
+        # Save samples for plotting
+        if i % int(1/sample_rate) == 0:
+            data_out[sample_number, 0] = dt*i
+            data_out[sample_number, 1:] = u_new[bd_size:-bd_size]
+            sample_number += 1
+
+        # Update old array with new array
+        u_old = u_new
+
+        if i % int(1e6) == 0:
+            print(f'Process: {i/Nt*100} %')
+    
+    # Save data
+    temp_time_to_run = "{:e}".format(time_to_run)
+    np.savetxt(f"""data/udata_ny{ny}_t{temp_time_to_run}_n_f{n_forcing}_f{int(forcing.real)}_j{int(forcing.imag)}.csv""", data_out,
+                delimiter=",",
+                header=f"""f={forcing}, n_f={n_forcing}, ny={ny},
+                            time={time_to_run}, dt={dt}, epsilon={epsilon},
+                            lambda={lambda_const}""")
+
+ 
+# Run ny
+# for ny in [1e-6, 1e-7, 1e-8]:
+#     print(f'Running on ny={ny}')
+run_model()
+
+# # Reset ny
+# ny = 0
+# # Run forcing
+# forcing = 10 + 10j
+# for n_forcing in [0, 2, 4]:
+#     print(f'Running on n_forcing={n_forcing}')
+#     run_model()
+
